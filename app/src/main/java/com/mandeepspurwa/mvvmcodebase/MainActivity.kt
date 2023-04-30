854bb161cb3a6e169c576b76c42b2611765ae127
Mandeep Singh <officialmandeepsp@gmail.com>
2023-04-30 22:19:40 +0530

    :rocket: adapter generic extension

--- app/src/main/java/com/mandeepspurwa/mvvmcodebase/MainActivity.kt
+++ app/src/main/java/com/mandeepspurwa/mvvmcodebase/MainActivity.kt
@@ -13,25 +13,12 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-
-//        binding.rcyName.newbindData(
-//            data = listOf("item1", "item2", "item3", "item4", "item4", "item4", "item4", "item14"),
-////            VB = ActivityMainBinding.inflate(LayoutInflater.from(baseContext)),
-//            layoutInflator = ItemTextBinding::inflate,
-//            VB = ItemTextBinding,
-//            bindFun = { binding, item ->
-//                binding.apply {
-//
-//                }
-//
-//            }
-//        ) { item -> Log.d("TAG", "onCreate() called with: item = $item") }
-//        val itemBinding = ItemTextBinding.inflate(layoutInflater,View, false)
-//        binding.rcyName.newbindData<String, ItemTextBinding>(
-//            itemBinding,
-//            data = listOf("item1", "item2", "item3", "item4", "item4",  "item14")
-//        ) { binding, item ->
-//            binding.textView.text = item
-//        }
+        val itemBinding = ItemTextBinding.inflate(layoutInflater)
+        binding.rcyName.newbindData<String, ItemTextBinding>(
+            itemBinding,
+            data = listOf("item1", "item2", "item3", "item4", "item4",  "item14")
+        ) { binding, item ->
+            binding.textView.text = item
+        }
     }
 }
--- app/src/main/java/com/mandeepspurwa/mvvmcodebase/util/extension/ViewExtension.kt
+++ app/src/main/java/com/mandeepspurwa/mvvmcodebase/util/extension/ViewExtension.kt
@@ -13,6 +13,7 @@ import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
 import androidx.viewbinding.ViewBinding
 import coil.load
+import java.util.Objects
 
 /**
  * App Name: MVVM Codebase
@@ -44,32 +45,6 @@ fun View.gone() {
     this.visibility = View.GONE
 }
 
-fun <T> RecyclerView.bindData(
-    data: List<T>,
-    layoutRes: Int,
-    bindFun: (View, T) -> Unit,
-    clickListener: ((T) -> Unit)? = null
-) {
-    adapter = object : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
-        inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
-
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-            val view = LayoutInflater.from(parent.context).inflate(layoutRes, parent, false)
-            return ViewHolder(view)
-        }
-
-        override fun getItemCount(): Int = data.size
-
-        override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-            val item = data[position]
-            Log.d("TAG", "onBindViewHolder() called with: holder = $holder, position = $position")
-            bindFun(holder.itemView, item)
-            clickListener?.let { listner ->
-                holder.itemView onClick { listner(item) }
-            }
-        }
-    }
-}
 
 class ViewHolder<K : ViewBinding>(val binding: K) : RecyclerView.ViewHolder(binding.root)
 
@@ -77,40 +52,16 @@ fun <T, K : ViewBinding> RecyclerView.newbindData(
     itemBinding: K,
     data: List<T>,
     bindFun: (viewBinding: K, listType: T) -> Unit,
-
 ) {
     adapter = object : RecyclerView.Adapter<ViewHolder<K>>() {
-
-
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder<K> {
             return ViewHolder(itemBinding)
         }
-
         override fun getItemCount(): Int = data.size
-
         override fun onBindViewHolder(holder: ViewHolder<K>, position: Int) {
             val listItem = data[position]
             bindFun(holder.binding, listItem)
         }
 
-    }
-}
-
-
-fun Activity.withPermissions(vararg permissions: String, callback: () -> Unit) {
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-        val ungrantedPermissions = permissions.filter {
-            checkSelfPermission(it) == PackageManager.PERMISSION_DENIED
-        }
-        if (ungrantedPermissions.isEmpty()) {
-            // All permissions are granted, execute callback
-            callback()
-        } else {
-            // Request permissions
-            requestPermissions(ungrantedPermissions.toTypedArray(), 0)
-        }
-    } else {
-        // Pre-Marshmallow devices, execute callback
-        callback()
     }
 }
